750a9690052dd92007918011fdc1dba27ecdbc7a
Shawn Bellazan <shawn_bellazan@outlook.com>
2024-11-08 07:22:15 -0600

    Add project files.

---
+++ src/YourProject.Shared/Models/Prompt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourProject.Shared.Models
+{
+    public abstract class Prompt
+    {
+        public abstract string GetPrompt();
+    }
+
+    public sealed class ProgressCheckPrompt : Prompt
+    {
+        public override string GetPrompt() => "Where am I in the current project, and what’s next?";
+    }
+
+    public sealed class MotivationRequestPrompt : Prompt
+    {
+        public override string GetPrompt() => "Remind me why this project is important.";
+    }
+
+    public sealed class TemplateCodeReuseReminderPrompt : Prompt
+    {
+        public override string GetPrompt() => "Do I have anything in my repo that I can reuse here?";
+    }
+
+    public sealed class QuickStartPrompt : Prompt
+    {
+        public override string GetPrompt() => "Give me a quick step to get going on the next task.";
+    }
+}
